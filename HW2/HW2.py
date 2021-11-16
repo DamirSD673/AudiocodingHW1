dc409c1dc1127f354a9a100e0927c0623721332c
DamirSD673 <vspishka.sveta673@gmail.com>
2021-11-16 13:17:00 +0300

    edited:Added legends

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -5,7 +5,7 @@ import sound as snd
 
 #  Task 2
 # Ramp function
-x = np.arange(44100) / 44100
+x = np.arange(2048) / 2048
 # MDCT
 N = 512
 Y = DirectMDCT(x, N)
@@ -16,15 +16,14 @@ X_hat = SyntesisMDCT(Y, N)
 fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12, 5))
 ax.plot(x)
 ax.plot(X_hat)
-ax.legend("Original", "Reconstructed")
-ax.title("Original and Reconstructed signals")
 ax.set_xlabel("Samples, n")
 ax.set_ylabel("Amplitude")
+ax.legend(["Original", "Reconstructed"])
 
 # Task 3
 [s, rate, bytes] = snd.wavread('Track16.wav')
 N_s = len(s)
-seconds = 8
+seconds = 2
 s_hat = s[int(N_s/2):int(N_s/2)+rate*seconds] # elements from middle and 8 sec
 s_hat = s_hat[:, 0]
 
@@ -32,11 +31,10 @@ Y_s = DirectMDCT(s_hat, N)
 S_hat = SyntesisMDCT(Y_s, N)
 
 fig, ax2 = plt.subplots(nrows=1, ncols=1, figsize=(12, 5))
-ax2.plot(s_hat[0:len(s_hat) - 1023], 'r--', linewidth=2)
-ax2.plot(S_hat[1023::], linewidth=0.5)
-ax2.legend("Original", "Reconstructed")
-ax2.title("Original and Reconstructed signals")
+ax2.plot(s_hat, 'r--', linewidth=2, label='Original')
+ax2.plot(S_hat, linewidth=0.5, label="Reconstructed")
 ax2.set_xlabel("Samples, n")
 ax2.set_ylabel("Amplitude")
+ax2.legend(["Original", "Reconstructed"])
 
 plt.show()
